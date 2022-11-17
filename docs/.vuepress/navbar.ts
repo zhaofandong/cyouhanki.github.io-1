778bbc797f905b102f7ffb3abfdc6affbe9c7626
cyouhanki <118335691+cyouhanki@users.noreply.github.com>
2022-11-17 13:49:52 +0900

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,7 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
   { text: "博客", icon: "blog", link: "/blog" },
-  {
+  /*{
     text: "代码",
     icon: "code",
     prefix: "/",
@@ -52,5 +52,5 @@ export default navbar([
       "Coupon",
     ],
   },
-  { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
+  { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },*/
 ]);
